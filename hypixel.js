0a91ac3668a399c2488244743f818c8e64fa64c6
Henry-Santa <flashducky4@gmail.com>
2022-03-25 21:00:17 -0400

    adding in some classes

--- hypixel.js
+++ hypixel.js
@@ -1,11 +1,11 @@
-class Hypixel{
+export class Hypixel{
     // creates a new instance of the class
     constructor(apiKey = ""){
         this.name = "hypixel.js";
         this.version = "1.0.0";
         this.author = "Henry-Santa";
         this.description = "A hypixel api wrapper in javascript meant to be used in a website.";
-        this.url = "";
+        this.url = "https://github.com/Henry-Santa/hypixel.js";
         this.apiKey = apiKey;
         this.apiUrl = "https://api.hypixel.net/";
         this.hasApiKey = false;
@@ -37,6 +37,18 @@ class Hypixel{
         }
         return json.uuid;
     };
+    // returns all the whole player's stats
+    async getPlayer(uuid){
+        if (!this.hasApiKey){
+            return "No api key";
+        }
+        let response = await fetch(this.apiUrl + `player?uuid=${uuid}&key=${this.apiKey}`);
+        let json = await response.json();
+        if (json.success){
+            return json.player;
+        }
+        return "No player found";
+    }
     // gets the player's stats
     async getPlayersGameStats(uuid, game){
         if (!this.hasApiKey){
@@ -56,9 +68,52 @@ class Hypixel{
         "GingerBread","Housing","HungerGames",
         "Legacy","MCGO","MurderMystery",
         "Paintball","Pit","Quake",
-        "SkyBlock" /* I Recommend using the dedicated Skyblock apis */,"SkyClash","SkyWars",
+        "SkyBlock" /* only gets profile ID's */,"SkyClash","SkyWars",
         "SuperSmash","TNTGames","TrueCombat",
         "UHC","VampireZ","Walls",
         "Walls3"]
+    }
+    async createHypixelSkyblock(){
+        return new HypixelSkyblock(this.apiKey);
+    }
+}
+
+export class HypixelSkyblock{
+    constructor(apiKey = ""){
+        this.apiKey = apiKey;
+        this.apiUrl = "https://api.hypixel.net/";
+        this.hasApiKey = false;
+        if (this.apiKey != ""){
+            this.hasApiKey = true;
+        }
+    }
+    async getSkyblockProfileData(skyProfileId){
+        if (!this.hasApiKey){
+            return "No api key";
+        }
+        let response = await fetch(`${this.apiUrl}skyblock/profile/${skyProfileId}`);
+        let json = await response.json();
+        if (json.error){
+            return json.error;
+        }
+        return json;
+    }
+    async getPlayersProfiles(uuid){
+        // returns an array of all the players profiles
+        if (!this.hasApiKey){
+            return "No api key";
+        }
+        let response = await fetch(`${this.apiUrl}skyblock/profiles?uuid=${uuid}?key=${this.apiKey}`);
+        let json = await response.json();
+        if (json.success){
+            return json.profiles;
+        }
+        return "No player found";
+    }
+}
+
+export class HypixelBazaar{
+    constructor(){
+        this.apiUrl = "https://api.hypixel.net/";
     }
 }
