45da5a1da41b966ffe6509d6a519d8ea3eaa71e2
Henry-Santa <flashducky4@gmail.com>
2022-03-31 19:36:17 -0400

    giggity, giggity goo (I have been watching a lot of family guy)

--- nbtParser.js
+++ nbtParser.js
@@ -1,6 +1,37 @@
-// Maybe a nbt parser in javascript if I have time
+// Maybe an nbt parser in javascript if I have time
 
 class nbt{
+    lookUpTable = {
+		'end': 0,
+		'byte': 1,
+		'short': 2,
+		'int': 3,
+		'long': 4,
+		'float': 5,
+		'double': 6,
+		'byteArray': 7,
+		'string': 8,
+		'list': 9,
+		'compound': 10,
+		'intArray': 11,
+		'longArray': 12
+	};
+    reverseLookUpTable = {
+        0: 'end',
+        1: 'byte',
+        2: 'short',
+        3: 'int',
+        4: 'long',
+        5: 'float',
+        6: 'double',
+        7: 'byteArray',
+        8: 'string',
+        9: 'list',
+        10: 'compound',
+        11: 'intArray',
+        12: 'longArray'
+    };
+    
     /**
      * @param {any} data compressed data, (string), or uncompressed (unit8array)
      * @param {boolean} isCompressed
@@ -20,5 +51,5 @@ class nbt{
     async __decompress(){
 		var head = new Uint8Array(this.data.slice(0, 2));
 		this.data = head.length === 2 && head[0] === 0x1f && head[1] === 0x8b;
-    }
+    };
 }
