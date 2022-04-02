6212b50d2012755f5e4b94df2bfd5439dae7fbc1
Henry-Santa <flashducky4@gmail.com>
2022-04-01 22:58:42 -0400

    Merge branch 'main' of https://github.com/Henry-Santa/hypixel.js

--- nbtParser.js
+++ nbtParser.js
@@ -46,36 +46,73 @@ class nbt{
         if(this.isCompressed){
             // decompress the data
             this.data = await this.__decompress();
+            console.log(this.data);
+        } else {
+            this.__Parse(this.data);
         }
-        // parse the data
-        return await this.__Parse(this.data, 0);
+        // parse the data 
+        
     }
     // a function that decompresses a gzipped string into a unit8 array
     async __decompress(){
         // create a blob
         var blob = new Blob([this.data]);
-        // decompress the blob (it is gzipped data)
-        var data = await (await fetch(URL.createObjectURL(blob))).arrayBuffer();
-        // return the data
-        return new Uint8Array(data);
+        const ds = new DecompressionStream('gzip');
+        // use the blob and the decompression stream to create a unit8array
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(blob);
+        reader.onload = async () => {
+            const buffer = await reader.result;
+            const unit8 = new Uint8Array(buffer);
+            this.data = unit8;
+            await this.__Parse(unit8);
+        }
+        
+        
+    
     }
     async __Parse(data){
+        console.log(data);
         let tags = {};
         // get the type of the tag
-        for(var i = 0; i < data[0]; i++){
-            tags[i] = {
-                type: data[i],
-                name: data[i+1],
-                value: data[i+2]
-            };
+        for(let i=0; i<data.length; i++){
+            if (data[i] === 0) /*compound tag*/{
+                let indexAfterName = data[i+1];
+                let compondedData = new Uint8Array();
+                for (let j=i+1; j<data.length; j++){
+                tags[String.fromCharCode(data[data[i+1]])] = compondedData;
+                i+=data[i+1];
+            }
         }
         console.log(tags);
     }
+}}
+class nbtCompound{
+    constructor(name, data, indexStart){
+        this.name = name;
+        this.data = data;
+    }
+    async getInsideData(){
+        let data = this.data;
+        let tags = {};
+        // get the type of the tag
+        for(let i=0; i<data.length; i++){
+            if (data[i] === 0) /*compound tag*/{
+                let indexAfterName = data[i+1];
+                let compondedData = new Uint8Array();
+                for (let j=i+1; j<data.length; j++){
+                tags[String.fromCharCode(data[data[i+1]])] = compondedData;
+                i+=data[i+1];
+            }
+        }
+        return tags;
+    }}
 }
 
 //test
 async function test(){
-    var data = "H4sIAAAAAAAAAO1Xy1LbVhg+YEiM00w6k0vTTpuctEmnHaJGd1veGdvYbm2ZYIPBG+ZIOrIFsuTRBTC77rrprtvsOsML9Al4gy676+QJ+gSd/kcyJEAIaTZlOvHG1tH33z7934fIIbSAZpwcQmhuFs061gw3g+bLfuxFMzmUichgAc1RzxwyxMwsyri7LsokSJRFM+jGmmcElOwQw6UzGbRQdyy67JJBCPC/c+i65YRjl0wgSdMPaBZOH6Ivjw7znSig3iAaFvHRobkoKezrm5pvuRO8KPDfoieAKQdOhMtD4pk0hSlPXocpT16DVciIDKawwilYAWAI3QVgnRI3rUgWBShZX0G34bhCbeqFdHrOK+gxHDa8iLquM6DT4mTxVIsKy3nn6FCr0HE0xDANzB3gRqORTLdKrdikIR76e3gUm0M88WNMAopD19+jFnoAGMfD0ZDiPRJBoDFhNQSeZwN9l2RepgTuB3iZQB/eAK8nmRueCXSH09xDZ5DmNomHbQCih4AxqA1c44jssDh2jK2EHiiTMAalFPjCxLNwMG31DOxmChOVtKEb0FAt8Pdg1rSPWkC8KEyIySfMvHzxK04JTgJuQcBK4EfUjBzfg6CvTgcJLIi8fPETntKfRCHWv7ocQysdGuEl34vDImPXh0GWXN+3EGYkjMaBvwtNMwKhUCkcQx3s2+yAVdKqnpUMaDiuE00YvU5KHGOEgVgaeMJ07AcRhiWN2JZNH8OiCDeTKQ3XN3fCp2eCsRUHhI2FHjFMGiOxeiE1fc8KE2IHzi7g0dcpkYtTkn7Ex5uf9OeywfacaJhsIssNo1ALG2zyR+neqkeHbrNaq+qV0uomXmq3u50Mmjd91w/QX78/zqI5nYwo+hyQ6XZO+aoEZADUL/l+FKIculXdjwJSimBVjTiiYfY4R06U+aLIa0VJzaLsyLcc26EBmh+wZFm04AfOwPG6ZIDulldLy92GXtuqrTYqW+V2s1ktd7OJGdzudFfbcKeyWqq19a2kzRz6iFkHPPQRhQefQTctJpetMJULjJfJoFt2uuhbdrrocDqfQbnxyfKkB9cGyfqxCzCeuTiGmo+pbEiCKEpcoZCXOVm085wmkjxHBEuSDInXeIOHXMTbddytGFQD4bMwUuSMKDzy0Rh9rD0TpWeChgW+KIi41AIAupa6CQMnlvj0zZaI2Od9LPCTq2GB+QssUHl3C/zgC/+RL9w/6wvwqwbKfGUNf/7y6bE1PLzAGpp0AMoeXOIOolwU5EJRVC5xh9un3aFTbyxPveHeaW84bvWcPbym8XMO8Er0isoLAi+bnEwliZNJXuQ0xaacWjBoQdEszZS0S0Qv8M+0RPRiUZTxyhtFv/j/FL12gejzH0R/5UX/2VnRl+vVTnelWepWT2T/x4PSsewfXSD78hB0ANsZ0bcJf0EU8kWJL6qX6f76mv6D3u7pqdbvn9b6qw7Pqf3d/sQT1dKIxIucpBBQuyRbHNFsgxO1PK/ZimSKknK52guJ2oWiol6g9p/fovbz8s51dmAt23seDWDqBrRpqnnBkGTKGYIscRJvS5yhqhanSIJgywWb2qYKcaCCMQ0ih4YLKBvR/SgOaJj8awW8rxM3pug3Ovme728MeWvje9ecNFS47nZ4t93YHoM81ydGuaE2RnC/vnTQdgq7Vm1dturrk/5GKzZ6btzv6ZPNXp9vjhTXKmsnuZoHprw5aol6d21/c7TutLarfLv2XGkduI6+vaO0ttf29crSTmu7JbV7fbdV6w83t/uuLj4/aPU2Rb23OtK7S47ehdF7m5IuVvf63eeOvSFoMMFVNj35AtMTP5jelTe9e2dNr15ttqrdY5P74gKTq1N3RKM3Gty/cLM7p90sLf2e7y1CQaUaZW8rBdng5IJlcQVL5TklX9B4uNQEmn+7k906eW8pCmeNLIPQP0GCADjMEQAA";
+    console.log(window.zlib);
+    var data = "H4sIABSUR2IA/wEhAN7/CgALaGVsbG8gd29ybGQIAARuYW1lAAlCYW5hbnJhbWEAd9pcOiEAAAA=";
     var b64Decode = atob(data);
     var NBT = new nbt(b64Decode,true);
     NBT.parse();
