29dd920f4af5b6d13b4a0f3351081e8323d374f8
Henry-Santa <flashducky4@gmail.com>
2022-04-13 18:11:55 -0400

    better structuring (NBT)

--- nbtParser.js
+++ nbtParser.js
@@ -1,37 +1,141 @@
 // Maybe an nbt parser in javascript if I have time
+const lookUpTable = {
+    'end': 0,
+    'byte': 1,
+    'short': 2,
+    'int': 3,
+    'long': 4,
+    'float': 5,
+    'double': 6,
+    'byteArray': 7,
+    'string': 8,
+    'list': 9,
+    'compound': 10,
+    'intArray': 11,
+    'longArray': 12
+};
+const reverseLookUpTable = {
+    0: 'end',
+    1: 'byte',
+    2: 'short',
+    3: 'int',
+    4: 'long',
+    5: 'float',
+    6: 'double',
+    7: 'byteArray',
+    8: 'string',
+    9: 'list',
+    10: 'compound',
+    11: 'intArray',
+    12: 'longArray'
+};
+class nbtTag{
+    constructor(type, name, value){
+        this.type = type;
+        this.name = name;
+    }
+    getType(){
+        return this.type;
+    }
+    getName(){
+        return this.name;
+    }
+    getValue(){
+        return this.value;
+    }
+    toString(){
+        return this.name + ': ' + this.value;
+    }
+}
+class nbtCompound extends nbtTag{
+    /**
+     * 
+     * @param {String} name 
+     * @param {Number} startIndex 
+     * @param {Unit8Array} data 
+     */
+    constructor(name, startIndex, data, parent=null){
+        super(10, name);
+        this.value = {};
+        this.currIndex = startIndex;
+        this.data = data;
+        this.parent = parent;
+    }
+    async parseInside(){
+        // get the type of the tag
+        let type = this.data[this.currIndex];
+        this.currIndex++;
+        if(type === 0){
+            // end tag
+            return;
+        }
+        // get the length of the name
+        let lenOfName = this.data[this.currIndex];
+        this.currIndex++;
+        // get the name of the tag
+        let name = String.fromCharCode(this.data.splice(this.currIndex, lenOfName));
+        this.currIndex += lenOfName;
+        if (type==10){
+            // if the tag is a compound
+            this.value[name] = new nbtCompound(name, this.currIndex, this.data, this);
+            await this.value[name].parseInside();
+        }
+    }
+}
+class nbtList extends nbtTag{
+    constructor(name, startIndex, data, parent=null, typeOList=9){
+        super(typeOList, name);
+        this.value = [];
+        this.currIndex = startIndex;
+        this.data = data;
+        this.parent = parent;
+        this.insideType = 0;
+    }
+    async parseInside(){
+        let type = this.data[this.currIndex];
+        if(type === 10){
+            // if the tag is a compound
+            this.value.push(new nbtCompound(null, this.currIndex, this.data, this));
+            await this.value[this.value.length-1].parseInside();
+            this.insideType = 10;
+        }
+        else if (type === 8){
+            // if the tag is a string
+            let lenOfString = this.data[this.currIndex+1];
+            let Strng = String.fromCharCode(this.data.splice(this.currIndex+2, lenOfString));
+            this.value.push(new nbtTag(8, null,Strng));
+            this.currIndex += lenOfName+2;
+            this.insideType = 8;
+        }else if (type === 1){
+            // if the tag is a byte
+            this.value.push(new nbtTag(1, null, this.data[this.currIndex+1]));
+            this.insideType = 1;
+        } else if (type === 2){
+            // if the tag is a short
+            this.value.push(new nbtTag(2, null, this.data[this.currIndex+1] + this.data[this.currIndex+2]*256));
+            this.insideType = 2;
+
+        } else if (type === 3){
+            // if the tag is a int
+            this.value.push(new nbtTag(3, null, this.data[this.currIndex+1] + this.data[this.currIndex+2]*256 + this.data[this.currIndex+3]*65536 + this.data[this.currIndex+4]*16777216));
+            this.insideType = 3;
+        } else if (type === 4){
+            // if the tag is a long
+            this.value.push(new nbtTag(4, null, this.data[this.currIndex+1] + this.data[this.currIndex+2]*256 + this.data[this.currIndex+3]*65536 + this.data[this.currIndex+4]*16777216 + this.data[this.currIndex+5]*4294967296 + this.data[this.currIndex+6]*1099511627776 + this.data[this.currIndex+7]*281474976710656));
+            this.insideType = 4;
+        } else if (type === 5){
+            // if the tag is a float
+            this.value.push(new nbtTag(5, null, this.data[this.currIndex+1] + this.data[this.currIndex+2]*256 + this.data[this.currIndex+3]*65536 + this.data[this.currIndex+4]*16777216));
+            this.insideType = 5;
+        } else if (type === 6){
+            // if the tag is a double
+            this.value.push(new nbtTag(6, null, this.data[this.currIndex+1] + this.data[this.currIndex+2]*256 + this.data[this.currIndex+3]*65536 + this.data[this.currIndex+4]*16777216 + this.data[this.currIndex+5]*4294967296 + this.data[this.currIndex+6]*1099511627776 + this.data[this.currIndex+7]*281474976710656));
+            this.insideType = 6;
+        } else if (type === 7 or type === 8, 9, 11, 12){
+            // if the tag is some sort of array or list
+    }
 
 class nbt{
-    lookUpTable = {
-		'end': 0,
-		'byte': 1,
-		'short': 2,
-		'int': 3,
-		'long': 4,
-		'float': 5,
-		'double': 6,
-		'byteArray': 7,
-		'string': 8,
-		'list': 9,
-		'compound': 10,
-		'intArray': 11,
-		'longArray': 12
-	};
-    reverseLookUpTable = {
-        0: 'end',
-        1: 'byte',
-        2: 'short',
-        3: 'int',
-        4: 'long',
-        5: 'float',
-        6: 'double',
-        7: 'byteArray',
-        8: 'string',
-        9: 'list',
-        10: 'compound',
-        11: 'intArray',
-        12: 'longArray'
-    };
-    
     /**
      * @param {any} data compressed data, (string), or uncompressed (unit8array)
      * @param {boolean} isCompressed if the data is compressed
@@ -66,50 +170,17 @@ class nbt{
             const unit8 = new Uint8Array(buffer);
             this.data = unit8;
             await this.__Parse(unit8);
-        }
-        
-        
-    
-    }
+        }}
+    /**
+     * 
+     * @param {Uint8Array} data 
+     */
     async __Parse(data){
-        console.log(data);
-        let tags = {"main": new nbtCompound()};
-        let currComp = tags.main;
-        currComp.parent = tags;
-        let insideComp = 1; // the og compound
-        let currIndex = 0;
-        // get the type of the tag
-        while(insideComp){
-            let type = data[currIndex];
-            let nameAsString = "";
-            let name = data.splice(currIndex+1,currIndex+data[currIndex+1]);
-            for(let i = 0; i < name.length; i++){
-                nameAsString += String.fromCharCode(name[i]);
-            }
-            if(type === 0){
-                insideComp -= 1;
-                
-            } else if (type === 10){
-                insideComp += 1;
-                currComp = currComp.parent;
-
-                let oldParent = currComp;
-                currComp = new nbtCompound();
-                currComp.parent = oldParent;
-                oldParent.children[nameAsString] = currComp;
-            }
-            
-            // I CANT FIGURE THIS OUT
-        console.log(tags);
-        }
-    }
-}
-class nbtCompound{
-    constructor(){
-        this.children = {};
-        this.parent = null;
-    }
-}
+        let lenOfName = data[1];
+        // get the name of the tag
+        let rootName = String.fromCharCode(data.splice(2, lenOfName+2));
+        let ourRoot = new nbtCompound(rootName, lenOfName+3);
+}};
 
 //test
 async function test(){
